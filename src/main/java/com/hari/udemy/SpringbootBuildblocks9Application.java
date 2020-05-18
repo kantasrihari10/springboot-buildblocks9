997baab301cf10ff9edcd37f39b8b46c050b1956
sriharinayudu <sriharinayudut@xyz>
2020-05-18 14:46:50 +0530

    first commit build9

---
+++ src/main/java/com/hari/udemy/SpringbootBuildblocks9Application.java
@@ -0,0 +1,13 @@
+package com.hari.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildblocks9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildblocks9Application.class, args);
+	}
+
+}
